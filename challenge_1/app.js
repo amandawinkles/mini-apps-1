a2734c1a9fca22f13096d3c903936834bed585c8
Amanda Winkles <amandawinkles@gmail.com>
2020-06-29 17:17:09 -0500

    setup init func and click event listener

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,16 +4,20 @@
 
 //the app detects a win or tie and displays an appropriate message
 
-var players, activePlayer;
+var players, activePlayer, win, tie;
 
 init();
+
+//new game button calls init func
+document.querySelector('.reset-button').addEventListener('click', init);
 
 //reset game by initializing app w/ init function
 function init() {
   //array to store players?
   players = [playerX, playerO];
   activePlayer = players[0];
-
+  win = 0;
+  tie = 0;
 };
 
 
