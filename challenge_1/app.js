a76c76a71a05162f1922b41fe67ee6f07c6967da
Amanda Winkles <amandawinkles@gmail.com>
2020-06-29 16:42:21 -0500

    setting up init function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,19 @@
+//game reset (button click) must not be done by refreshing the page
+
+//the first move always starts with player X
+
+//the app detects a win or tie and displays an appropriate message
+
+var players, activePlayer;
+
+init();
+
+//reset game by initializing app w/ init function
+function init() {
+  //array to store players?
+  players = [playerX, playerO];
+  activePlayer = players[0];
+
+};
 
 
