8243e8e0955fcd916a23ba1f035c3f62ad32706e
Amanda Winkles <amandawinkles@gmail.com>
2020-07-01 10:05:53 -0500

    makeMove function working

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,30 +4,56 @@
 
 //the app detects a win or tie and displays an appropriate message
 
-var players, playerX, playerO, activePlayer, win, tie;
+var players, playerX, playerO, activePlayer, win, tie, piece, cells;
 
 init();
 
-//make move func
-  //activePlayer
+document.querySelectorAll("#board td").forEach((cell) => cell.addEventListener("click", function(cell) {
+  makeMove(cell);
+}));
 
-//pass to next player func
+//make move func
+function makeMove(cell) {
+  if (activePlayer === playerX) {
+    //console.log('activePlayer in makeMove', activePlayer);
+    //place x in cell
+    piece = 'X';
+    document.getElementById(event.target.id).innerText = piece;
+    //activePlayer set to playerO
+    activePlayer = playerO;
+  } else {
+    //place o in cell
+    piece = 'O';
+    document.getElementById(event.target.id).innerText = piece;
+    //activePlayer set to playerX
+    activePlayer = playerX;
+  }
+};
 
 //win or tie func
   //message displayed
   //end ability to make a move
 
 //new game button calls init func
-document.querySelector(".reset-button").addEventListener('click', init);
+document.querySelector(".reset-button").addEventListener("click", init);
 
 //reset game by initializing app w/ init function
 function init() {
-  //array to store players?
-  players = [playerX, playerO];
-  activePlayer = players[0];
+  playerX = document.getElementById("player-x");
+  playerO = document.getElementById("player-o");
+  activePlayer = playerX;
   win = 0;
   tie = 0;
-
-  document.querySelector("#x").style.display = 'none';
-  document.querySelector('#o').style.display = 'none';
+  cells = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
 };
+
+//console.log('activePlayer', activePlayer);
+
+// document.querySelector("#x").style.display = "none";
+// document.querySelector('#o').style.display = "none";
+
+//if cell is empty or '', cells[cellClicked] = activePlayer;
