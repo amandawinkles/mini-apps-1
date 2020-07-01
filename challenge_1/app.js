a4f99dc0f5773109130fa6736dbbee93e40c1522
Amanda Winkles <amandawinkles@gmail.com>
2020-07-01 18:18:19 -0500

    just have to get conditional statement for tie to work

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,7 +4,7 @@
 
 //the app detects a win or tie and displays an appropriate message
 
-var players, playerX, playerO, activePlayer, win, tie, piece, board, alertMessage;
+var players, playerX, playerO, activePlayer, win, tie, piece, board, alertMessage, win;
 
 init();
 
@@ -31,35 +31,31 @@ function makeMove(cell) {
 };
 
 board = document.querySelectorAll("#board td");
-console.log('board', board);
+//console.log('board', board);
 //console.log('board at 0', board[0]);
 
 alertMessage = document.querySelector("h2");
+console.log('alertMessage', alertMessage);
 
 function alertWinOrTie(finalState) {
   if (finalState === 'win') {
-    //set textContent on alertMessage
-    //<h2>WIN!</h2>
     alertMessage.textContent = 'W I N';
+    console.log('alertMessage', alertMessage);
   } else if (finalState === 'tie') {
-    //<h2>TIE!</h2>
     alertMessage.textContent = 'T I E';
+  } else if (finalState === '') { //why is alertMessage undefined after this?
+    document.querySelector("h2").textContent = '';
   }
 };
 
 //trigger winOrTie to get called after 3 pieces from same player are played OR after each turn
 board.forEach((cell) => cell.addEventListener("click", winOrTie));
-//addEventListener("click", winOrTie);
-// .forEach((cell) => cell.addEventListener("click", function(cell) {
-//   makeMove(cell);
-// }));
 
 //win or tie func
 function winOrTie(board) {
   board = document.querySelectorAll("#board td");
   console.log('board', board);
-  //players = [playerX, playerO];
-  //possible wins stored in obj
+
   var possibleWins = {
     0: [board[0], board[1], board[2]],
     1: [board[3], board[4], board[5]],
@@ -70,42 +66,39 @@ function winOrTie(board) {
     6: [board[0], board[4], board[8]],
     7: [board[2], board[4], board[6]]
   };
-  //if possibleWins.0 === [X, X, X] || [O, O, O], call alertWinOrTie with 'win'
-  if (possibleWins['0'] === ['X', 'X', 'X'] || ['O', 'O', 'O']) { //possibleWins.0 === [O, O, O]
-    alertWinOrTie('win');
-  } else if (possibleWins['1'] === ['X', 'X', 'X'] || ['O', 'O', 'O']) { //possibleWins.1 === [O, O, O]
-    alertWinOrTie('win');
-  } else if (possibleWins['2'] === ['X', 'X', 'X'] || ['O', 'O', 'O']) { //possibleWins.2 === [O, O, O]
-    alertWinOrTie('win');
-  } else if (possibleWins['3'] === ['X', 'X', 'X'] || ['O', 'O', 'O']) { //possibleWins.3 === [O, O, O]
-    alertWinOrTie('win');
-  } else if (possibleWins['4'] === ['X', 'X', 'X'] || ['O', 'O', 'O']) { //possibleWins.4 === [O, O, O]
-    alertWinOrTie('win');
-  } else if (possibleWins['5'] === ['X', 'X', 'X'] || ['O', 'O', 'O']) { //possibleWins.5 === [O, O, O]
-    alertWinOrTie('win');
-  } else if (possibleWins['6'] === ['X', 'X', 'X'] || ['O', 'O', 'O']) { //possibleWins.6 === [O, O, O]
-    alertWinOrTie('win');
-  } else if (possibleWins['7'] === ['X', 'X', 'X'] || ['O', 'O', 'O']) { //possibleWins.7 === [O, O, O]
-    alertWinOrTie('win');
-  }
-  //if board has no empty cells, fall through to tie
-  //loop through board cells and check if no cells contain empty string
-  board = Array.from(board);
-  console.log('board', board);
-  for (let i = 0; i < board.length; i++) {
-    if (board[i].innerText === '') {
-      return;
-    } else {
-      continue;
-    }
 
-  }
-  //alertWinOrTie('tie');
+  win = false;
 
-  //need to get out of func to get back to play?
+  if (((possibleWins['0'][0].innerText === 'X') && (possibleWins['0'][1].innerText === 'X') && (possibleWins['0'][2].innerText === 'X')) || ((possibleWins['0'][0].innerText === 'O') && (possibleWins['0'][1].innerText === 'O') && (possibleWins['0'][2].innerText === 'O'))) {
+    alertWinOrTie('win');
+    win = true;
+  } else if (possibleWins['1'] === ['X', 'X', 'X'] || possibleWins['1'] === ['O', 'O', 'O']) {
+    alertWinOrTie('win');
+    win = true;
+  } else if (possibleWins['2'] === ['X', 'X', 'X'] || possibleWins['2'] === ['O', 'O', 'O']) {
+    alertWinOrTie('win');
+    win = true;
+  } else if (possibleWins['3'] === ['X', 'X', 'X'] || possibleWins['3'] === ['O', 'O', 'O']) {
+    alertWinOrTie('win');
+    win = true;
+  } else if (possibleWins['4'] === ['X', 'X', 'X'] || possibleWins['4'] === ['O', 'O', 'O']) {
+    alertWinOrTie('win');
+    win = true;
+  } else if (possibleWins['5'] === ['X', 'X', 'X'] || possibleWins['5'] === ['O', 'O', 'O']) {
+    alertWinOrTie('win');
+    win = true;
+  } else if (possibleWins['6'] === ['X', 'X', 'X'] || possibleWins['6'] === ['O', 'O', 'O']) {
+    alertWinOrTie('win');
+    win = true;
+  } else if (possibleWins['7'] === ['X', 'X', 'X'] || possibleWins['7'] === ['O', 'O', 'O']) {
+    alertWinOrTie('win');
+    win = true;
+  }
+
+  if (((!!possibleWins['0'].includes('')) && (!!possibleWins['1'].includes('')) && (!!possibleWins['2'].includes('')) && (!!possibleWins['3'].includes('')) && (!!possibleWins['4'].includes('')) && (!!possibleWins['5'].includes('')) && (!!possibleWins['6'].includes('')) && (!!possibleWins['7'].includes(''))) && (win === false)) {
+    alertWinOrTie('tie');
+  }
 };
-  //message displayed
-  //end ability to make a move
 
 //new game button calls init func
 document.querySelector(".reset-button").addEventListener("click", init);
@@ -116,8 +109,7 @@ function init() {
   playerX = document.getElementById("player-x");
   playerO = document.getElementById("player-o");
   activePlayer = playerX;
-  win = 0;
-  tie = 0;
+  alertWinOrTie('');
 };
 
 //console.log('activePlayer', activePlayer);
@@ -126,12 +118,3 @@ function init() {
 // document.querySelector('#o').style.display = "none";
 
 //if cell is empty or '', piece can be set;
-
-//cells,
-// cells = [
-//   '', '', '',
-//   '', '', '',
-//   '', '', ''
-//   ];
-
-//<h2>Win!</h2>
