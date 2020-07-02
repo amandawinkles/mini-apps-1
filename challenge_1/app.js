a57e4c368bf680eea9af9d045c44eb0796e0168b
Amanda Winkles <amandawinkles@gmail.com>
2020-07-01 22:04:36 -0500

    Complete Tic Tac Toe

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -9,30 +9,30 @@ document.querySelectorAll("#board td").forEach((cell) => cell.addEventListener("
 }));
 
 function makeMove(cell) {
-  //If the position is already occupied, don't place an X or O and do not move on to the next player's turn
   if (activePlayer === playerX) {
     piece = 'X';
-    document.getElementById(event.target.id).innerText = piece;
-    playCount++;
-    activePlayer = playerO;
+    if (document.getElementById(event.target.id).innerText === '') {
+      document.getElementById(event.target.id).innerText = piece;
+      playCount++;
+      activePlayer = playerO;
+    }
   } else {
     piece = 'O';
-    document.getElementById(event.target.id).innerText = piece;
-    playCount++;
-    activePlayer = playerX;
+    if (document.getElementById(event.target.id).innerText === '') {
+      document.getElementById(event.target.id).innerText = piece;
+      playCount++;
+      activePlayer = playerX;
+    }
   }
 };
 
 board = document.querySelectorAll("#board td");
-//console.log('board', board);
 
 alertMessage = document.querySelector("h2");
-//console.log('alertMessage', alertMessage);
 
 function alertWinOrTie(finalState) {
   if (finalState === 'win') {
     alertMessage.textContent = 'W I N';
-    //console.log('alertMessage', alertMessage);
   } else if (finalState === 'tie') {
     alertMessage.textContent = 'T I E';
   } else if (finalState === '') { //why is alertMessage undefined after this?
@@ -44,7 +44,6 @@ board.forEach((cell) => cell.addEventListener("click", winOrTie));
 
 function winOrTie(board) {
   board = document.querySelectorAll("#board td");
-  console.log('board', board);
 
   var possibleWins = {
     0: [board[0], board[1], board[2]],
@@ -59,7 +58,6 @@ function winOrTie(board) {
 
   win = false;
 
-  //if 5 or more plays have been made
   if (playCount >= 5) {
     if (((possibleWins['0'][0].innerText === 'X') && (possibleWins['0'][1].innerText === 'X') && (possibleWins['0'][2].innerText === 'X')) || ((possibleWins['0'][0].innerText === 'O') && (possibleWins['0'][1].innerText === 'O') && (possibleWins['0'][2].innerText === 'O'))) {
       alertWinOrTie('win');
@@ -94,16 +92,14 @@ function winOrTie(board) {
       win = true;
     }
   }
-  //if 9 plays have been made && win === false
+
   if ((playCount === 9) && (win === false)) {
     alertWinOrTie('tie');
   }
 };
 
-//new game button calls init func
 document.querySelector(".reset-button").addEventListener("click", init);
 
-//reset game by initializing app w/ init function
 function init() {
   document.querySelectorAll("#board td").forEach((cell) => cell.innerText = '');
   playerX = document.getElementById("player-x");
@@ -111,14 +107,3 @@ function init() {
   activePlayer = playerX;
   alertWinOrTie('');
 };
-
-//console.log('activePlayer', activePlayer);
-//console.log('activePlayer in makeMove', activePlayer);
-//console.log('board at 0', board[0]);
-
-// if (((!!possibleWins['0'].includes('')) && (!!possibleWins['1'].includes('')) && (!!possibleWins['2'].includes('')) && (!!possibleWins['3'].includes('')) && (!!possibleWins['4'].includes('')) && (!!possibleWins['5'].includes('')) && (!!possibleWins['6'].includes('')) && (!!possibleWins['7'].includes(''))) && (win === false))
-
-// document.querySelector("#x").style.display = "none";
-// document.querySelector('#o').style.display = "none";
-
-//if cell is empty or '', piece can be set;
