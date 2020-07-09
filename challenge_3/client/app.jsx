aa254e731fad28ed5ea3ada2595599141611236b
Amanda Winkles <amandawinkles@gmail.com>
2020-07-08 23:31:53 -0500

    setting up files and planning

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 
+// class MyComponent extends React.Component {
+//   render() {
+//     return <div>Hello World</div>;
+//   }
+// }
+
+// ReactDOM.render(<MyComponent />, document.getElementById('app'));
+
+//Link your transpiled component file from index.html
+
+//when Checkout button clicked, takes the user to the first of several forms. We'll call the forms F1, F2, F3
+//At each step, a Next button allows the user to progress to the next step in the checkout process
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const port = process.env.PORT || 4568;
 
+app.get('/', function (req, res) {
+  res.send('hello world')
+});
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
+
+module.exports = app;
 
