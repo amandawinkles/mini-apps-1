37c362d56b3799d1585f9b2bce0f827b2c9e5929
Amanda Winkles <amandawinkles@gmail.com>
2020-07-09 23:54:54 -0500

    setup db, implemented homepage component

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,16 +1,75 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+//components = Checkout, F1, F2, F3, Page4
+//store data in db for each checkout experience
 
-// class MyComponent extends React.Component {
-//   render() {
-//     return <div>Hello World</div>;
-//   }
-// }
+//Homepage class component /homepage
+  //render CheckoutButton
+class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {movingToF1: false};
+    this.handleCheckoutClick = this.handleCheckoutClick.bind(this);
+  }
 
-// ReactDOM.render(<MyComponent />, document.getElementById('app'));
+  handleCheckoutClick(e) {
+    e.preventDefault();
+    this.setState({movingToF1: true});
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={props.onClick}>
+         Checkout
+        </button>
+      </div>
+    );
+  }
+}
+
+//F1 class component
+  //render F1
+  //render NextButton
+
+//F2 class component
+  //render F2
+  //render NextButton
+
+//F3 class component
+  //render F3
+  //render NextButton
+
+//Page4 class component
+  //render Page4
+  //render PurchaseButton
+
+ReactDOM.render(<Homepage />, document.getElementById('app'));
 
 //Link your transpiled component file from index.html
 
-//when Checkout button clicked, takes the user to the first of several forms. We'll call the forms F1, F2, F3
-//At each step, a Next button allows the user to progress to the next step in the checkout process
+//when Checkout button clicked, takes the user to the first of several forms -> F1, F2, F3, (Page4)
+
+
+
+
+// function PurchaseButton(props) {
+//   return (
+//     <button onClick={props.onClick}>
+//       Purchase
+//     </button>
+//   );
+// }
+
+// function NextButton(props) {
+//   return (
+//     <button onClick={props.onClick}>
+//       Next
+//     </button>
+//   );
+// }
+
+//const url = 'http://localhost/4568';
+
+
+//<button onClick={this.deleteRow.bind(this, id)}>Delete Row</button>
+
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -2,16 +2,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const React = require('react');
+const ReactDOM = require('react-dom');
 const ReactDOMServer = require('react-dom/server');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 4568;
+//const components = require('./client/app');
+
+//app.use('/', components);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
+  //get components
   res.send('hello world')
 });
 
 app.get('/', (req, res) => res.render('index'));
 
+app.post('/', (req, res) => {
+  console.log('req.body', req.body);
+});
+
 app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
 
 module.exports = app;
-
